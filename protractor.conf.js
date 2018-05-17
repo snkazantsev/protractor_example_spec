321c7e2c8ab03d26d6be9b162b224a1143249254
snkazantsev <s.n.kazantsev@gmail.com>
2018-05-17 16:09:42 +0700

    added headless mode settings

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,8 +1,18 @@
 // conf.js
 exports.config = {
+  capabilities: {
+        // 'browserName': 'firefox'
+    browserName: 'chrome',
+    chromeOptions: {
+      // run in headless mode
+      args: ['--headless', '--verbose', '--privileged', '--no-sandbox', '--disable-gpu', '--window-size=1280,800', 'disable-infobars=true' ],
+      prefs: {
+        credentials_enable_service: false,
+      },
+     }
+  },
   framework: 'jasmine',
   directConnect: true,
   specs: ['specs/**/*.spec.js'],
   baseUrl: 'http://www.thetestroom.com/jswebapp/'
 }
-
