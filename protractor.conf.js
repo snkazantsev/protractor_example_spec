40f5b9c3da40bb758bfc7d776a94632a7ce2a77e
snkazantsev <s.n.kazantsev@gmail.com>
2018-06-29 14:18:15 +0700

    started with cucumber

--- protractor.conf.js
+++ protractor.conf.js
@@ -10,8 +10,20 @@ exports.config = {
       },
      }
   },
-  framework: 'jasmine',
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {
+    format:  'pretty',
+    require: 'specs/step_definitions/*.js', // This is where we'll be writing our actual tests
+  },
+
+  params: {
+    env: {
+      hostname: 'http://0.0.0.0:8000' // Whatever the address of your app is
+    }
+  },
+
   directConnect: true,
-  specs: ['specs/**/*.spec.js'],
+  specs: ['specs/features/**/*.feature'],
   baseUrl: 'http://www.thetestroom.com/jswebapp/'
 }
